90a0a5186365f57b6b720da9475d46efe3d38750
ehmPlankje <root@dev.plankje.eu>
2016-01-07 04:36:25 +0100

    Minimized Angular

--- public/media/js/controller.js
+++ public/media/js/controller.js
@@ -2,85 +2,49 @@
 var app = angular.module('app', [ 'ui.bootstrap']);
 
 app.controller('CountryController', function($scope, $http, $uibModal) {
-    $scope.search = '';
-    $scope.create = function() {
-        $uibModal.open({
-            templateUrl: '/template/country/create.html',
+    function modal(country, action) {
+        $scope.country = country;
+    
+        return $uibModal.open({
+            templateUrl: '/template/country/' + action + '.html',
             controller: 'CountryModalController',
             size: 'md',
-            scope: $scope,
-            resolve: {
-                country: null
-            }
+            scope: $scope
         });
     };
     
-    $scope.read = function(country) {
-        $uibModal.open({
-            templateUrl: '/template/country/read.html',
-            controller: 'CountryModalController',
-            size: 'md',
-            scope: $scope,
-            resolve: {
-                country: country
-            }
-        });
-    }
-    
-    $scope.update = function(country) {
-        $uibModal.open({
-            templateUrl: '/template/country/update.html',
-            controller: 'CountryModalController',
-            size: 'md',
-            scope: $scope,
-            resolve: {
-                country: country
-            }
-        });
-    }
-    
-    $scope.delete = function(country) {
-        $uibModal.open({
-            templateUrl: '/template/country/delete.html',
-            controller: 'CountryModalController',
-            size: 'md',
-            scope: $scope,
-            resolve: {
-                country: country
-            }
-        });
-    }
-    
+    $scope.search = '';
+    $scope.create = function() { return modal(null, 'create'); }
+    $scope.read = function(country) { return modal(country, 'read'); }
+    $scope.update = function(country) { return modal(country, 'update'); }
+    $scope.delete = function(country) { return modal(country, 'delete'); }
     $scope.refresh = function() {
+        $scope.country = null;
+        $scope.countries = null;
         $http.get('/api/country/search/name/' + $scope.search).success(function(response) {
             $scope.countries = response.data;
         });
     }
 });
 
-app.controller('CountryModalController', function($scope, $http, $uibModalInstance, country) {
-    $scope.country = country;
+app.controller('CountryModalController', function($scope, $http, $uibModalInstance) {
+    function after() {
+        $scope.refresh();
+        $uibModalInstance.close();
+    }
 
     $scope.create = function () {
-        $http.put('/api/country/create', $scope.country).then(function() {
-            $scope.refresh();
-            $uibModalInstance.close();
-        });
+        $http.put('/api/country/create', $scope.country).then(after);
     };
 
     $scope.update = function () {
-        $http.patch('/api/country/update/' + $scope.country.id, $scope.country).then(function() {
-            $scope.refresh();
-            $uibModalInstance.close();
-        });
+        $http.patch('/api/country/update/' + $scope.country.id, $scope.country).then(after);
     };
     
     $scope.delete = function () {
-        $http.delete('/api/country/delete/' + $scope.country.id).then(function() {
-            $scope.refresh();
-            $uibModalInstance.close();
-        });
+        $http.delete('/api/country/delete/' + $scope.country.id).then(after);
     };
+    
     $scope.dismiss = function () {
         $scope.refresh();
         $uibModalInstance.dismiss();
